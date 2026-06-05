18eaa2fadb2e04dd4e93050a9e7e3eb5dff621e5
kenshiha <kyokasuigetsu979@gmail.com>
2026-06-05 22:37:43 +0530

    Linked based Queue Implement working

--- src/Queue/LinkedListQueue.java
+++ src/Queue/LinkedListQueue.java
@@ -1,9 +1,24 @@
 package Queue;
 
 public class LinkedListQueue {
-
+        private Node first;
+        private Node rear;
+        int size;
 
     private class Node{
+            int val;
+            Node next;
 
+            Node(int val){
+                this.val = val;
+                next = null;
+            }
     }
+
+    LinkedListQueue(){
+        first = null;
+        rear = null;
+        size = 0;
+    }
+
 }
