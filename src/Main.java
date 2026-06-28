e7125f1e108da2255e47d099b35480d097cbd3bd
kenshiha <kyokasuigetsu979@gmail.com>
2026-06-28 23:19:38 +0530

    Remove element fix

--- src/Main.java
+++ src/Main.java
@@ -6,24 +6,21 @@ public class Main {
 
     public static int bst(int[] nums,int target){
 
-      int left = 0;
-      int right = nums.length - 1;
+        int left = 0;
+        int right = nums.length - 1;
 
-      while(left <= right){
+        while(left <= right){
+            int middle = left + ( right - left) / 2;
 
-          int middle = left + (right - left) /2;
-
-          if(nums[middle] == target ){
-              return middle;
-          } else if (nums[middle] < target) {
-              left = middle + 1;
-          }else {
-              right = middle - 1;
-          }
-      }
-
-
-    return -1;
+            if(nums[middle] == target){
+                return middle;
+            } else if (nums[middle] > target) {
+                right = middle - 1;
+            }else {
+                left = middle + 1;
+            }
+        }
+        return -1;
     }
     public static void main(String[] args) {
 
