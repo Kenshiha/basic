422875b133af19be56418ba6b17a0325592c9a9f
kenshiha <kyokasuigetsu979@gmail.com>
2026-07-09 00:14:46 +0530

    Graph add/remove edge

--- src/Graph/Graph.java
+++ src/Graph/Graph.java
@@ -4,6 +4,28 @@ import java.util.ArrayList;
 
 public class Graph {
 
+    ArrayList<ArrayList<Integer>> graph;
+
+    Graph(int vertices){
+        graph = new ArrayList<>();
+
+        for(int i = 0; i < vertices; i++){
+            graph.add(new ArrayList<>());
+        }
+    }
+
+    // Two Way connection
+    void addEdges(int u,int v){
+        graph.get(u).add(v);
+        graph.get(v).add(u);
+    }
+
+    void removeEdge(int u, int v){
+        graph.get(u).remove(Integer.valueOf(v));
+        graph.get(v).remove(Integer.valueOf(v));
+    }
+
+
     public void main(String[] args){
 
         ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
