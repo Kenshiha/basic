870a673397376a4ad8cea6827b8a8272a6410a58
kenshiha <kyokasuigetsu979@gmail.com>
2026-07-09 17:05:59 +0530

    BFS/DFS

--- src/Graph/Graph.java
+++ src/Graph/Graph.java
@@ -1,6 +1,8 @@
 package Graph;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Graph {
 
@@ -22,18 +24,57 @@ public class Graph {
 
     void removeEdge(int u, int v){
         graph.get(u).remove(Integer.valueOf(v));
-        graph.get(v).remove(Integer.valueOf(v));
+        graph.get(v).remove(Integer.valueOf(u));
     }
+    void printGraph(){
 
+        for(int i = 0; i < graph.size();i++){
+            System.out.println(i + "-->" + graph.get(i));
+        }
+    }
+    void dfs(int vertex, boolean[] visited){
+        visited[vertex] = true;
+        System.out.print(vertex + " ");
 
-    public void main(String[] args){
-
-        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-
-        for(int i = 0; i < 5;i++){
-            graph.add(new ArrayList<>());
+        for(int neighbor : graph.get(vertex)){
+            if(!visited[neighbor]){
+                dfs(neighbor,visited);
+            }
         }
 
-        graph.get(0).add(1);
+    }
+
+    void bfs(int start){
+        boolean[] visited = new boolean[graph.size()];
+        Queue<Integer> queue = new LinkedList<>();
+
+        visited[start] = true;
+        queue.add(start);
+
+        while (!queue.isEmpty()){
+            int vertex = queue.poll();
+            System.out.print(vertex + " ");
+
+            for(int neighbor : graph.get(vertex)){
+                if(!visited[neighbor]){
+                    visited[neighbor] = true;
+                    queue.add(neighbor);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args){
+
+        Graph g = new Graph(5);
+
+        g.addEdges(0, 1);
+        g.addEdges(0, 2);
+        g.addEdges(1, 3);
+        g.addEdges(2, 4);
+
+        g.printGraph();
+        boolean[] visited = new boolean[g.graph.size()];
+        g.dfs(0,visited);
     }
 }
