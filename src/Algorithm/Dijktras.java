7012603d1a12d96f68c74cecf82e83cd1580cf89
kenshiha <kyokasuigetsu979@gmail.com>
2026-07-19 22:26:32 +0530

    Graph adjency Matrix

---
+++ src/Algorithm/Dijktras.java
@@ -0,0 +1,23 @@
+package Algorithm;
+
+import java.util.ArrayList;
+
+class graph{
+     ArrayList<ArrayList<Integer>> graph;
+
+     graph(int vertices){
+         graph = new ArrayList<>();
+
+         for(int i = 0; i < vertices; i++){
+             graph.add(new ArrayList<>());
+         }
+     }
+ }
+
+public class Dijktras {
+
+
+    public static void main(String[] args){
+
+    }
+}
