68d57f7fa6cc98710392e2aab608d535d3294ddb
kenshiha <kyokasuigetsu979@gmail.com>
2026-07-20 23:04:19 +0530

    Dijktras algorithm

--- src/Algorithm/Dijktras.java
+++ src/Algorithm/Dijktras.java
@@ -1,20 +1,92 @@
 package Algorithm;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
-class graph{
-     ArrayList<ArrayList<Integer>> graph;
 
-     graph(int vertices){
-         graph = new ArrayList<>();
+class Edge{
+    int dest;
+    int weight;
+
+    Edge(int dest, int weight){
+        this.dest = dest;
+        this.weight = weight;
+    }
+}
+
+class Graph{
+     ArrayList<ArrayList<Edge>> adjlst;
+
+     Graph(int vertices){
+         adjlst = new ArrayList<>();
 
          for(int i = 0; i < vertices; i++){
-             graph.add(new ArrayList<>());
+             adjlst.add(new ArrayList<>());
          }
+     }
+
+     void addEdge(int src, int dest, int weight){
+         adjlst.get(src).add(new Edge(dest,weight));
+     }
+     void removeEdge(int src, int dest, int weigth){
+         adjlst.get(src).removeIf(edge -> edge.dest == dest);
      }
  }
 
+ class Pair implements Comparable<Pair> {
+    int vertex;
+    int distance;
+
+    Pair(int vertex, int distance){
+        this.vertex = vertex;
+        this.distance = distance;
+    }
+    @Override
+    public int compareTo(Pair other){
+        return Integer.compare(this.distance, other.distance);
+    }
+
+ }
+
 public class Dijktras {
+
+    static void dijktras(Graph graph,int source){
+
+        int n = graph.adjlst.size();
+
+        int[] dist = new int[n];
+        boolean[] visited = new boolean[n];
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+
+        Arrays.fill(dist,Integer.MAX_VALUE);
+
+        dist[source] = 0;
+        pq.add(new Pair(source,0));
+
+        while (!pq.isEmpty()){
+
+            Pair current = pq.poll();
+            int u = current.vertex;
+
+            if(visited[u]){
+                continue;
+            }
+            visited[u] = true;
+
+            for(Edge edge : graph.adjlst.get(u)){
+                int v = edge.dest;
+                int weight = edge.weight;
+
+                if(dist[u] + weight < dist[v]){
+                    dist[v] = dist[u] + weight;
+                    pq.add(new Pair(v, dist[v]));
+                }
+            }
+        }
+    }
+
 
 
     public static void main(String[] args){
