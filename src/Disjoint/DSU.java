4460d0fd5482d9b00bc2cf2792a91876557c4f5c
kenshiha <kyokasuigetsu979@gmail.com>
2026-07-26 23:00:31 +0530

    Prim algorithm

--- src/Disjoint/DSU.java
+++ src/Disjoint/DSU.java
@@ -4,29 +4,25 @@ public class DSU {
 
     int[] parent;
 
-    DSU(int n){
-        parent = new int[n];
-
-        for(int i = 0; i < n; i++){
-            parent[i] = i;
+        DSU(int n){
+            parent = new int[n];
         }
-    }
 
-    int find(int x){
-        if(parent[x] == x){
-            return x;
+        int find(int x){
+            if(parent[x]==x){
+                return x;
+            }
+            return find(parent[x]);
         }
-        return find(parent[x]);
-    }
 
-    void  union(int a,int b){
-        int rootA = find(a);
-        int rootB = find(b);
+        void union(int a, int b){
+            int rootA = find(a);
+            int rootB = find(b);
 
-        if(rootA != rootB){
-            parent[rootB] = rootA;
+            if(rootA != rootB){
+                parent[rootB] = rootA;
+            }
         }
-    }
 
     public static void main(String[] args){
 
