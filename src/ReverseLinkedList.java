4186f6184dde79d98cd580a263ce2fb37b280027
kenshiha <kyokasuigetsu979@gmail.com>
2026-08-12 23:09:08 +0530

    heap sort

--- src/ReverseLinkedList.java
+++ src/ReverseLinkedList.java
@@ -36,6 +36,7 @@ public class ReverseLinkedList {
         Node head = new Node(1,new Node(2,new Node(3,new Node(4,new Node(5,new Node(6))))));
 
         System.out.println("Before Reverse");
+
         Node before = head;
         while (before != null){
             System.out.print(before.val + "->");
