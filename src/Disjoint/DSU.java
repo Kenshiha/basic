557860936b42ea958d677e45658d182d4f1f8c5d
kenshiha <kyokasuigetsu979@gmail.com>
2026-09-12 23:48:16 +0530

    member

--- src/Disjoint/DSU.java
+++ src/Disjoint/DSU.java
@@ -2,11 +2,13 @@ package Disjoint;
 
 public class DSU {
     int[] parent;
+    int[] rank;
 
         DSU(int n){
             parent = new int[n];
 
             for(int i = 0; i < n; i++){
+
                 parent[i] = i;            }
         }
         int find(int x){
